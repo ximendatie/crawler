aad2a318cccec7e500e5e64d1a5a150be885ff2f
harveyhe <harveyhe@tencent.com>
2019-08-14 22:30:19 +0800

    初始化index文件

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+
+const app = express()
+let server = app.listen(3000, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log('Your App is running at http://%s:%s', host, port);
+});
+
+app.get('/', function (req, res) {
+res.send('Hello World!');
+});
